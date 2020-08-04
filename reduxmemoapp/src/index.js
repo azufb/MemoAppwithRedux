7d811f358e0c1ad394c483e2e4891f8a40b107e8
Azusa Okamoto <curiousazunas@gmail.com>
2020-08-04 19:47:25 +0900

    index.jsとApp.jsの書き換え

--- reduxmemoapp/src/index.js
+++ reduxmemoapp/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import MemoStore from './Store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store = { MemoStore }>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
